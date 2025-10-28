959845281741ab72d18df94a2a05842cfb5c2c0b
@numinousv <@wvrdian333@proton.me>
2025-10-28 11:39:03 +0100

    aaaa

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const BASE_URL = "https://api.agify.io/";
 
-async function fetchData() {}
+async function fetchData() {
+    const button = document.getElementById("button");
+    button.addEventListener("click", async (event) => {
+        event.preventDefault();
+        const name = document.getElementById("name").value;
+        const response = await fetch(`${BASE_URL}?name=${name}`);
+        const data = await response.json();
+        console.log(data);
+        const resultElement = document.querySelector("p");
+        resultElement.textContent = data.name + " is " + data.age + " years old";
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchData();
+});
